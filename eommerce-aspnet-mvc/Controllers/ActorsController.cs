281e28e3de03aed8750634c6618f687aad84837f
kiethuynh <huynhtuankiet6496@gmail.com>
2023-06-20 11:29:09 +0700

    CRU opertions for actor

--- eommerce-aspnet-mvc/Controllers/ActorsController.cs
+++ eommerce-aspnet-mvc/Controllers/ActorsController.cs
@@ -1,5 +1,6 @@
 ﻿using eTickets.Data;
 using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -18,8 +19,58 @@ namespace eTickets.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var getActors = await _actorService.GetAll();
+            var getActors = await _actorService.GetAllAsync();
             return View(getActors);
+        }
+
+        // Get: Actors/Create
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("FullName,ProfilePictureURL,Bio")]Actor actor)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _actorService.AddAsync(actor);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // Get: Actors/Details/{id}
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _actorService.GetByIdAsync(id);
+            if(actorDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(actorDetails);
+        }
+
+        // Get: Actors/Update/{id}
+        public async Task<IActionResult> Edit(int id)
+        {
+            var actorDetails = await _actorService.GetByIdAsync(id);
+            if (actorDetails == null)
+            {
+                return View("NotFound");
+            }
+            return View(actorDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+            await _actorService.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
