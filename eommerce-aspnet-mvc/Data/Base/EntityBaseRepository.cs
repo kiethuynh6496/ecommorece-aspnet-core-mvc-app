0c1766ea7b402f095bc1c51ee7de10388eb044a1
kiethuynh <huynhtuankiet6496@gmail.com>
2023-06-22 10:14:11 +0700

    get all movies - add create page movie - set partial view for Add item

--- eommerce-aspnet-mvc/Data/Base/EntityBaseRepository.cs
+++ eommerce-aspnet-mvc/Data/Base/EntityBaseRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace eTickets.Data.Base
@@ -32,6 +33,13 @@ namespace eTickets.Data.Base
         {
             var listActors = await _context.Set<T>().ToListAsync();
             return listActors;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = _context.Set<T>();
+            query = includeProperties.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+            return await query.ToListAsync();
         }
 
         public async Task<T> GetByIdAsync(int id)
---
+++ eommerce-aspnet-mvc/Data/Services/MoviesService.cs
@@ -0,0 +1,43 @@
+﻿using eTickets.Data.Base;
+using eTickets.Data.ViewModels;
+using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public class MoviesService : EntityBaseRepository<Movie>, IMoviesService
+    {
+        private readonly AppDbContext _context;
+        public MoviesService(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Movie> GetMovieByIdAsync(int id)
+        {
+            var movieDetails = _context.Movies
+                .Include(c => c.Cinemas)
+                .Include(p => p.Producers)
+                .Include(am => am.Actors_Movies).ThenInclude(a => a.Actor)
+                .FirstOrDefaultAsync(n => n.Id == id);
+
+            return await movieDetails;
+        }
+
+        public async Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues()
+        {
+            var response = new NewMovieDropdownsVM()
+            {
+                Actors = await _context.Actors.OrderBy(n => n.FullName).ToListAsync(),
+                Cinemas = await _context.Cinemas.OrderBy(n => n.Name).ToListAsync(),
+                Producers = await _context.Producers.OrderBy(n => n.FullName).ToListAsync()
+            };
+
+            return response;
+        }
+    }
+}
