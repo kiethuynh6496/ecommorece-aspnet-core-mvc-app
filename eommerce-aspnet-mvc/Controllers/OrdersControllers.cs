27f1082013c6f12488616b9d2f06bb6aa97785fb
kiethuynh <huynhtuankiet6496@gmail.com>
2023-06-25 10:21:08 +0700

    add OrdersControllers

---
+++ eommerce-aspnet-mvc/Controllers/OrdersControllers.cs
@@ -0,0 +1,33 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicketBooking.Data.Services;
+
+namespace eTickets.Controllers
+{
+    public class OrdersControllers : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+        public OrdersControllers(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(response);
+        }
+    }
+}
